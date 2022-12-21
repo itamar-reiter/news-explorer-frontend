2092083e61105f48d7ea767c248f5266b3aa56e9
itamar reiter <itamarreiter@gmail.com>
2022-12-21 14:03:11 +0200

    create saved-news

--- news-explorer/src/components/SavedNewsTitles/SavedNewsTitles.js
+++ news-explorer/src/components/SavedNewsTitles/SavedNewsTitles.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import CurrentUserContext from '../../utils/CurrentUserContext';
 import './SavedNewsTitles.css';
 
 function SavedNewsTitles() {
+
+  const currentUserValue = useContext(CurrentUserContext);
   return (
-    <div>SavedNewsTitles</div>
+    <section className='saved-news-titles'>
+      <h1 className='saved-news-titles__title'>Saved articles</h1>
+      <p className='saved-news-titles__intro'>
+        {currentUserValue.name}, you have {currentUserValue.savedArticles} saved articles
+      </p>
+      <p className='saved-news-titles__keywords'>By keywords:&nbsp;
+        <span className='saved-news-titles__keywords_highlight'>
+         Nature, Yellowstone, and 2 other
+        </span>
+      </p>
+    </section>
   )
 }
 
