8eb589df0c69975cf3f504467a3e33e15e4918ce
itamar reiter <itamarreiter@gmail.com>
2023-02-05 12:26:58 +0200

    fixed server adress

--- news-explorer/src/utils/constants.js
+++ news-explorer/src/utils/constants.js
@@ -1,5 +1,3 @@
 export const apiData = {
-  baseUrl: "http://localhost:3001"
+  baseUrl: "https://api.itamarreiter.newsexplorer.students.nomoredomainssbs.ru",
 };
-
- /* token from practicum "4c5ffc10-7ef8-4216-9730-733ca7b03273" */
