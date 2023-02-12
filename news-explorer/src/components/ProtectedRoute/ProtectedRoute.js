97beac3f0b90c71fac99a7b4a653d87cc469302d
itamar reiter <itamarreiter@gmail.com>
2023-02-13 00:49:52 +0200

    changed route loading

--- news-explorer/src/components/ProtectedRoute/ProtectedRoute.js
+++ news-explorer/src/components/ProtectedRoute/ProtectedRoute.js
@@ -2,18 +2,18 @@ import React, {useState, useEffect } from "react";
 import { Route, Redirect } from 'react-router-dom';
 
 function ProtectedRoute({ children, loggedIn, redirectedPath, changeDirectionState, ...props }) {
-  const [isLoading, setIsLoading] = useState(true);
+  const [isRouteLoading, setIsRouteLoading] = useState(true);
 
   useEffect(() => {
     changeDirectionState();
     if (loggedIn !== undefined) {
-      setIsLoading(false);
+      setIsRouteLoading(false);
     }
   }, [changeDirectionState, loggedIn]);
-  
+
   return (
     <>
-    {isLoading ? <p>Loading...</p>
+    {isRouteLoading ? <p>Loading...</p>
     :<Route {...props}>
       {loggedIn === true && children}
       {loggedIn === false && <Redirect to={redirectedPath} />}
