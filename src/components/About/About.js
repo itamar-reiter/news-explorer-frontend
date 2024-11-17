fc8984eb752a800a6ebaf222c143924a3ccb83e8
itamar reiter <itamarreiter@gmail.com>
2024-11-17 13:01:30 +0200

    edit about me

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,12 +10,12 @@ function About() {
       <div className="about__content">
         <h2 className="about__title">About me</h2>
         <p className="about__description">
-          Hey there! I'm Itamar Reiter and I'm a Fullstack Developer.<br/>
+          Hey there! I'm Itamar Reiter and I'm a Software Developer.<br/>
           I Love building websites and learning new topics that can improve the development process and the product.<br/>
           I like to study subjects in depth and breadth, to specialize and invest.<br/>
           I have experience in website development, including server side and client side.<br/>
           I have a high work ethic, and I am willing to invest a lot for the success of the product.<br/>
-          Graduate of Yandex's practicum100 course.
+          Graduate of Yandex's triple10 Software Development course.
         </p>
       </div>
     </section>
